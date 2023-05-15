2beeffa18c4fc96fc26ecd345bb87da7b84a461b
keyli <keyli@Keylita>
2023-05-15 09:13:30 +0200

    Master de Keyla

---
+++ src/saludo/Saludo.java
@@ -0,0 +1,10 @@
+package saludo;
+
+public class Saludo {
+
+	public static void main(String[] args) {
+		System.out.println("Buenos días Ceinmark,soy Marta Gómez");
+		System.out.println("Introduce un número para averiguar si es primo o no");
+		
+	}
+}
